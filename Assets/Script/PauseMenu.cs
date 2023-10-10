73404a67f96f75d0b26cb0dc0e406d55e8c00210
Popey0424 <roylouis0424@gmail.com>
2023-10-10 13:33:37 +0200

    quelle que modification

--- Assets/Script/PauseMenu.cs
+++ Assets/Script/PauseMenu.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
 
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject MenuPause;
+    [SerializeField] private Image ImageFade;
 
     public void OnClickPause()
     {
@@ -16,4 +21,13 @@ public class PauseMenu : MonoBehaviour
         MenuPause.SetActive(false);
     }
 
+    public void OnClickLeave()
+    {
+        ImageFade.DOFade(1, 2.9f).OnComplete(FadeComplete);
+    }
+
+    private void FadeComplete()
+    {
+        SceneManager.LoadScene("Menu");
+    }
 }
